2dd798e88d57c17964f408255680f835cd4ea4b4
Punmanat <60070053@kmitl.ac.th>
2020-02-03 16:17:29 +0700

    Config redis

--- scg-backend/src/settings/redisClient.js
+++ scg-backend/src/settings/redisClient.js
@@ -1,5 +1,5 @@
 const redis = require("redis")
-const REDISHOST = process.env.REDISHOST || 'localhost';
+const REDISHOST = '10.10.58.219';
 const REDISPORT = process.env.REDISPORT || 6379;
 const client = redis.createClient(REDISPORT, REDISHOST);
 client.on('error', err => console.error('ERR:REDIS:', err));
